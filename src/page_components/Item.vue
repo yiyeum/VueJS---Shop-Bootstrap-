52b7744ae325ae64f40df988886009e646e5412c
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-08-30 13:13:29 -0700

    Item card using data
    -name
    -price
    -description
    -category

--- src/page_components/Item.vue
+++ src/page_components/Item.vue
@@ -4,10 +4,11 @@
             <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
             <div class="card-body">
                 <h4 class="card-title">
-                    <a href="#">Item One</a>
+                    <a href="#">{{name}}</a>
                 </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                <h5>{{price}}</h5>
+                <p class="card-text">{{description}}</p>
+                <p>{{category}}</p>
             </div>
             <div class="card-footer">
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -17,7 +18,16 @@
 </template>
 
 <script>
-    export default{}
+    export default{
+        data:function(){
+            return{
+                name : 'Item Name',
+                price : '$24.99',
+                description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+                category : 'Category1'
+            }
+        }
+    }
 </script>
 
 <style>
