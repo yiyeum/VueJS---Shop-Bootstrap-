dae183ecf052a48c3881e3ac0c71abbcb3983f76
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-08-30 13:51:06 -0700

    v-on on the button to convert between CAD and USD

--- src/page_components/Item.vue
+++ src/page_components/Item.vue
@@ -6,9 +6,11 @@
                 <h4 class="card-title">
                     <a href="#">{{name}}</a>
                 </h4>
-                <h5>{{price}}</h5>
+                <h5><span>{{priceSign}}</span> {{price}}</h5>
                 <p class="card-text">{{description}}</p>
-                <p>{{category}}</p>
+                <button type="button" class="btn btn-primary" v-on:click="convertCad">Convert to {{convertPrice}}</button>
+                <hr />
+                <span>{{category}}</span>
             </div>
             <div class="card-footer">
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -22,9 +24,26 @@
         data:function(){
             return{
                 name : 'Item Name',
-                price : '$24.99',
+                price : '24.99',
+                priceSign : 'CAD',
+                convertPrice : 'USD',
                 description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
-                category : 'Category1'
+                category : 'Category1',
+                CAD : true
+            }
+        },
+        methods:{
+            convertCad:function(){
+                if(this.CAD) {
+                    this.price = (this.price * 0.9132).toFixed(2);
+                    this.priceSign = "USD";
+                    this.convertPrice = "CAD";
+                }else{
+                    this.price = (this.price / 0.9132).toFixed(2);
+                    this.priceSign = "CAD";
+                    this.convertPrice = "USD";
+                }
+                this.CAD = !this.CAD;
             }
         }
     }
