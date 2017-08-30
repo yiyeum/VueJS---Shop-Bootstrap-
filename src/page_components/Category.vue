4999cf19df07bbf9bb2841c7e6fba6936b5b4db3
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-08-30 13:12:56 -0700

    List categories array using v-for

--- src/page_components/Category.vue
+++ src/page_components/Category.vue
@@ -3,16 +3,21 @@
 
         <h1 class="my-4">Shop Name</h1>
         <div class="list-group">
-            <a href="#" class="list-group-item">Category 1</a>
-            <a href="#" class="list-group-item">Category 2</a>
-            <a href="#" class="list-group-item">Category 3</a>
+            <a href="#" class="list-group-item" v-for='category in categories'>{{category}}</a>
         </div>
 
     </div>
 </template>
 
 <script>
-    export default{}
+    export default{
+        data:function(){
+            return{
+                shopName : "Shop Name",
+                categories :["Category 1", "Category 2", "Category 3"]
+            }
+        }
+    }
 </script>
 
 <style>
